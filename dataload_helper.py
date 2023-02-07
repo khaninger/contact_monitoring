23f4c37f645262b57fb8328282481127cce0b9c1
christian-hegeler <christian.hegeler@icloud.com>
2023-02-07 23:13:18 +0100

    dataloader for plug and rake with different functionalities; vizualizer for data

---
+++ dataload_helper.py
@@ -0,0 +1,130 @@
+import pickle
+import os
+import numpy as np
+from rotation_helpers import xyz_to_rotation
+
+class data():
+
+    def __init__(self, clustered):
+        self.directory = os.path.dirname(os.path.realpath(__file__)) + "/data/"
+        if clustered:
+            self.directory += "clustering_video_"
+        else:
+            self.directory += "video_"
+    def load_data(self, path):
+        return pickle.load(open(path, "rb"))
+
+    """
+    def play_video(self, path):
+        print(path)
+        os.system("start " + path)
+    """
+class plug(data):
+    def __init__(self, experiment="", index=1, clustered=False, segment=True):
+        # experiments are
+        # ""
+        # "less_"
+        # "threading_"
+        self.segment=segment
+        self.experiment = experiment
+        self.index = index
+        data.__init__(self, clustered=clustered)
+        self.directory += "plug_"
+        self.directory += self.experiment
+        self.path = self.directory + str(self.index) + ".pickle"
+
+    def load(self):
+        if os.path.isfile(self.path):
+            dataset = np.array(self.load_data(self.path))
+            print(f"\nLoaded {len(dataset)} samples from plug dataset {self.experiment}")
+            #generate list of segments
+            if self.segment:
+                dataset_segments = []
+                for segment in range(int(max(dataset[:, -1])) + 1):
+                    dataset_segments.append(dataset[dataset[:, -1] == segment, :3])
+                print(
+                    f"Dataset contains {len(dataset_segments)} segments:")
+                try:
+                    print(f"Segment 1: {len(dataset_segments[0])} samples")
+                    print(f"Segment 2: {len(dataset_segments[1])} samples")
+                    print(f"Segment 3: {len(dataset_segments[2])} samples")
+                except:
+                    pass
+
+                dataset = dataset_segments
+            return dataset
+    """
+    def video(self):
+        data.__init__(self, clustered=False)
+        self.directory += "plug_"
+        self.directory += self.experiment
+        self.path = self.directory + str(self.index) + ".mp4"
+        self.play_video(self.path)
+    """
+
+    def random(self, rest_pt = np.array([0.1, 0.2, 0.3]), pt = np.array([0., 0., 0.5]), noise=0.01, rot_0=.4, rot_1=.2):
+
+        Tmats = []
+        std = noise
+        mean = 0
+        noise_t = np.random.normal(mean, std, size=(3,))
+        noise_r = np.random.normal(mean, std, size=(3,))
+        for x_rot in np.linspace(-rot_0/2, rot_0/2, 100):
+            for y_rot in np.linspace(-rot_1/2, rot_1/2, 100):
+                rotation = np.array([x_rot, y_rot, 0])+noise_r
+                new_rotmat = xyz_to_rotation(rotation)
+                new_pt = new_rotmat @ pt + rest_pt
+                Tmat = np.eye(4)
+                Tmat[:3, 3] = np.squeeze(new_pt) + noise_t
+                Tmat[:3, :3] = new_rotmat
+                Tmats.append(Tmat)
+        Tmats=np.array(Tmats)
+        index = np.random.choice(len(Tmats), 25, replace=False).astype(int)
+
+        return Tmats[index]
+
+
+class rake(data):
+    def __init__(self, index=1, clustered=False, segment=True):
+        self.segment=segment
+        self.index = index
+        data.__init__(self, clustered=clustered)
+        self.directory += "rake_"
+        self.path = self.directory + str(self.index) + ".pickle"
+
+    def load(self, center=False):
+        if os.path.isfile(self.path):
+            dataset = np.array(self.load_data(self.path))
+            print(f"\nLoaded {len(dataset)} samples from rake dataset")
+            if center:
+                dataset = np.mean(dataset, axis=1)[:, None, :]
+
+            #generate list of segments
+            if self.segment:
+                dataset_segments = []
+                for segment in range(int(max(dataset[:, 0, -1])) + 1):
+                    dataset_segments.append(dataset[dataset[:, 0, -1] == segment, :, :3][:,0,:])
+                print(
+                    f"Dataset contains {len(dataset_segments)} segments:")
+                try:
+                    print(f"Segment 1: {len(dataset_segments[0])} samples")
+                    print(f"Segment 2: {len(dataset_segments[1])} samples")
+                    print(f"Segment 3: {len(dataset_segments[2])} samples")
+                except:
+                    pass
+
+                dataset = dataset_segments
+            return dataset
+
+
+
+if __name__ == "__main__":
+    for i in range(3):
+        for experiment in ["", "threading_", "less_"]:
+            dataset = plug(index=i+1, experiment="threading_", clustered=False, segment=True).load()
+    for i in range(5):
+        dataset = rake(index=i+1, clustered=False, segment=True).load(center=True)
+
+
+
+
--- visualize.py
+++ visualize.py
@@ -3,13 +3,63 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 def plot_3d_points(L,rest_pt):
-    points = np.array([arr[:3, 3] for arr in L])
+
+    if L[0].shape == (4,4):
+        points = np.array([arr[:3, 3] for arr in L])
+    else:
+        points = (np.array(L))
+
+
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(points[:, 0], points[:, 1], points[:, 2])
+    ax.scatter(points[:, 0], points[:, 1], points[:, 2], color='b')
     ax.scatter(rest_pt[0], rest_pt[1], rest_pt[2], color='r')
     plt.show()
 
+
+def plot_3d_points_segments(L, rest_pt, exp_n=0):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    color_segments = ['b', 'g', 'm']
+    for i, L_segment in enumerate(L):
+        if L_segment[0].shape == (4,4):
+            points = np.array([arr[:3, 3] for arr in L])
+        else:
+            points = (np.array(L_segment))
+        ax.scatter(points[:, 0], points[:, 1], points[:, 2], color=color_segments[i], label="segment "+str(i))
+    if np.mean(rest_pt) != 0:
+        ax.scatter(rest_pt[0], rest_pt[1], rest_pt[2], color='r', label="rest_pt")
+    if exp_n != 0:
+        ax.set_title(f"Experiment {exp_n}")
+    ax.legend()
+    plt.show()
+
 def plot_distance(L,rest_pt):
-    distances = np.array([np.linalg.norm(arr[:3, 3]-rest_pt) for arr in L])
+    if L[0].shape == (4, 4):
+        distances = np.array([np.linalg.norm(arr[:3, 3]-rest_pt) for arr in L])
+    else:
+        distances = np.array([np.linalg.norm(arr - rest_pt) for arr in L])
     print(f"distances: {distances}")
+
+
+if __name__ == "__main__":
+    from dataload_helper import plug, rake
+    show_plug = False
+    show_rake = True
+    if show_plug:
+
+        for i in range(3):
+            dataset = plug(index=1 + i, experiment="", clustered=False, segment=True).load()
+            plot_3d_points_segments(dataset, [-.3, -.3, 0])
+
+        for i in range(3):
+            dataset = plug(index=1 + i, experiment="less_", clustered=False, segment=True).load()
+            plot_3d_points_segments(dataset, [-.3, -.3, 0])
+
+        for i in range(3):
+            dataset = plug(index=1 + i, experiment="threading_", clustered=False, segment=True).load()
+            plot_3d_points_segments(dataset, [-.3, -.3, 0])
+    if show_rake:
+        for i in range(5):
+            dataset = rake(index=i+1, clustered=False, segment=True).load(center=True)
+            plot_3d_points_segments(dataset, [0,0,0], i+1)
