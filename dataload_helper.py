ca8ffd3671834ca245639b09febc42230e247e15
IPK410 <christian-hegelert-online.de>
2023-02-16 19:05:32 +0100

    first test of similarity, not sure if working correctly but number is changing when we push on it

--- dataload_helper.py
+++ dataload_helper.py
@@ -1,8 +1,8 @@
 import pickle
 import os
 import numpy as np
-from rotation_helpers import xyz_to_rotation
-import kp2pose
+from .rotation_helpers import xyz_to_rotation
+from .kp2pose import *
 
 class data():
 
@@ -154,13 +154,13 @@ class rake(data):
                 n_discard = 0
                 kp_dim = 3
                 init_kp = dataset[init_kp_index,:,:kp_dim]
-                T_init = kp2pose.init_T(init_kp)
+                T_init = init_T(init_kp)
                 __dataset = []
                 __dataset_segments = []
                 __dataset_time = []
                 for i in range(dataset.shape[0]):
                     data_kp = dataset[i,:,:kp_dim]
-                    T_rake, delta = kp2pose.find_T(init_kp, data_kp, kp_delta_th=kp_delta_th)
+                    T_rake, delta = find_T(init_kp, data_kp, kp_delta_th=kp_delta_th)
                     if not delta: # only append poses wher kp2kp transform results in per keypoint offset smaller than kp_delta_th
                         if pose:
                             __dataset.append(T_rake @ T_init)
